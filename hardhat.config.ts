10771e789f23dc7d915e4b33c04caf11fce1f54d
dmcong <congduong@descartes.network>
2023-11-24 11:40:19 +0700

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    mainnet: {
+      url: "https://rpc.tomochain.com",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    testnet: {
+      url: "https://rpc.testnet.tomochain.com",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
 };
 
 export default config;
