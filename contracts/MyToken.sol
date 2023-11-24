40743e0b1c1f07ca166c417b533b9289cd5ef570
dmcong <congduong@descartes.network>
2023-11-24 13:14:35 +0700

    3. init contract

---
+++ contracts/MyToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "vrc25/contracts/VRC25.sol";
+
+contract MyToken is VRC25 {
+  constructor() VRC25("MyToken", "MTK", 9) {
+    _mint(msg.sender, 10000);
+  }
+
+  function _estimateFee(uint256 value) internal view virtual override returns (uint256) {
+    return value * 0;
+  }
+}
---
+++ test/Amm.ts
@@ -0,0 +1,9 @@
+import { describe } from "mocha";
+
+// https://viction.tuphan.dev/
+
+describe("VictionAmm", function () {
+  it("Should OK", async function () {
+    // TODO: something
+  });
+});
