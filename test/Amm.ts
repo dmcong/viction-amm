e35106fea4758c8014de5e2275bef0300f8b5afe
dmcong <congduong@descartes.network>
2023-11-24 13:24:34 +0700

    4. init test

--- test/Amm.ts
+++ test/Amm.ts
@@ -1,9 +1,34 @@
+import { ethers } from "hardhat";
 import { describe } from "mocha";
 
 // https://viction.tuphan.dev/
 
 describe("VictionAmm", function () {
+  async function deployContractAmm() {
+    const [signer] = await ethers.getSigners();
+    const ammContract = await ethers.deployContract("Amm", [], {
+      gasLimit: 4000000,
+      signer,
+    });
+    await ammContract.waitForDeployment();
+    console.log(`AmmContract was deployed to ${ammContract.target}`);
+    return ammContract.target;
+  }
+
+  async function deployContractToken() {
+    const [signer] = await ethers.getSigners();
+    const tokenContract = await ethers.deployContract("MyToken", [], {
+      gasLimit: 4000000,
+      signer,
+    });
+    await tokenContract.waitForDeployment();
+    console.log(`TokenContract was deployed to ${tokenContract.target}`);
+    return tokenContract.target;
+  }
+
   it("Should OK", async function () {
+    const ammContract = await deployContractAmm();
+    const tokenContract = await deployContractToken();
     // TODO: something
   });
 });
