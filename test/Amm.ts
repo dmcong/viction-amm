b38c19e42b12d495ba1127012168e5d70fefd8d9
dmcong <congduong@descartes.network>
2023-11-25 07:07:16 +0700

    revert clean project

--- test/Amm.ts
+++
@@ -1,9 +0,0 @@
-import { describe } from "mocha";
-
-// https://viction.tuphan.dev/
-
-describe("VictionAmm", function () {
-  it("Should OK", async function () {
-    // TODO: something
-  });
-});
