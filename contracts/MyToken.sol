11ea31299eb78cabfae29b19d1100c21d1013d3e
dmcong <congduong@descartes.network>
2023-11-25 07:07:41 +0700

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import "vrc25/contracts/VRC25.sol";
 
 contract MyToken is VRC25 {
-  constructor() VRC25("MyToken", "MTK", 9) {
+  constructor() VRC25("MyToken", "MTK", 0) {
     _mint(msg.sender, 10000);
   }
 
